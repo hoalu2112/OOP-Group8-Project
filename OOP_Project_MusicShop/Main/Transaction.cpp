0951f03cb56b71057bdff2faa5b32a210c8b3abf
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2020-09-08 09:01:28 +0700

    cop phần cài đặt từ transaction.h qua transaction.cpp
    ban đầu t cài đặt trong file .h nên t cop qua cái cpp thôi chứ không có gì

--- OOP_Project_MusicShop/Main/Transaction.cpp
+++ OOP_Project_MusicShop/Main/Transaction.cpp
@@ -1,1 +1,36 @@
- 
+#include "Transaction.h"
+
+Transaction::Transaction() {
+	this->id = 0;
+	this->amount = 0;
+	this->total_price = 0;
+}
+
+Transaction::Transaction(const Transaction& p) {
+	this->id = p.id;
+	this->amount = p.amount;
+	this->total_price = p.total_price;
+	this->date = p.date;
+}
+
+string Transaction::toString() {
+	stringstream ss;
+	ss << this->id << "-" << this->date.toString() << "-" << this->amount << "-" << this->total_price;
+	return ss.str();
+}
+
+Transaction& Transaction::stringToTransaction(string transaction) {
+	vector<string> temp = Tokenizer::split(transaction, "-");
+	this->id = stoi(temp[0]);
+	this->date = this->date.parse(temp[1]);
+	this->amount = stoi(temp[2]);
+	this->total_price = stoi(temp[3]);
+
+	return *this;
+}
+void Transaction::printHistoryTransaction() {
+	cout << "ID: " << this->id << endl;
+	cout << "Transaction Date:" << this->date << endl;
+	cout << "Amount: " << this->amount << endl;
+	cout << "Total Price: " << this->total_price << endl << endl;
+}
